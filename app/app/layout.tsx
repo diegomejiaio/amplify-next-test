5906e545c119dfe0a50f3027815322cfc2d8c62e
Diego <diegomejiamagallanes7@gmail.com>
2024-08-29 13:35:52 -0500

    [feature] ready auth

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,37 +1,17 @@
-"use client";
-import React, { useState, useEffect } from "react";
-
-import AlertComponent from '@/components/AlertComponent';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { Toaster } from "@/components/ui/toaster"
-import { useToast } from "@/components/ui/use-toast"
-
+import React, { Suspense } from "react";
 
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const searchParams = useSearchParams();
-    const notification = searchParams.get('notification');
-    const { toast } = useToast()
-
-    useEffect(() => {
-        if (notification === 'npw') {
-            toast({
-                title: "Bienvenid@",
-                description: "Tu contraseña ha sido actualizada correctamente 🎉",
-                })
-            // limpiar la url
-            window.history.replaceState({}, document.title, "/app");
-        }
-    }, [notification]);
 
     return (
         <div>
             <main className="relative w-full h-screen flex items-center justify-center">
                 <div className="flex min-h-screen w-full flex-col bg-muted/40">
-                    {children}
+                    <Suspense>
+                        {children}
+                    </Suspense>
                 </div>
             </main>
-            <Toaster />
         </div>
 
 
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,26 +1,16 @@
 'use client';
 import '@aws-amplify/ui-react/styles.css';
 import { Suspense } from 'react';
-import { I18n } from 'aws-amplify/utils';
-import { translations } from '@aws-amplify/ui-react';
 import NavbarPublic from '../../components/NavbarPublic';
 import SpaceBackground from '../../components/SpaceBackground';
-import Signup from '../../components/Auth';
 import { Skeleton } from "@/components/ui/skeleton"
 import { Amplify } from 'aws-amplify';
 import outputs from "../../amplify_outputs.json";
 import Login from '@/app/auth/_components/Login';
-import Auth from '../../components/Auth';
 import { useState } from 'react';
 import ConfirmPassword from './_components/ConfirmPassword';
 import RecoverPassword from './_components/RecoverPassword';
-
-
-// Set language and configure Amplify
-I18n.putVocabularies(translations);
-I18n.setLanguage('es');
-
-
+import Auth from '@/components/Auth';
 
 Amplify.configure(outputs);
 
